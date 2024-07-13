799c25e430941d7fcdcdf60da9c6b8725b738ce1
igor_alves <igornalves08@gmail.com>
2024-07-13 12:53:42 -0300

    fazendo o tratamento de erros com o Sprig boot para identificar melhor a validacao dos campos selecionandos

--- src/main/java/br/com/igor/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/igor/gestao_vagas/GestaoVagasApplication.java
@@ -2,10 +2,10 @@ package br.com.igor.gestao_vagas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "br.com.igor.gestao_vagas")
+// @ComponentScan(basePackages = "br.com.igor.gestao_vagas")
 public class GestaoVagasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoVagasApplication.class, args);
--- src/main/java/br/com/igor/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/igor/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -14,15 +14,15 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] nao deve conter espaco")
+    @Pattern(regexp = "\\S+", message = "O campo [username] nao deve conter espaco")
     private String username;
 
     @Email(message = "o campo deve conter um e-mail valido")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "A senha deve conter entre (10) e (100) caracteres")
     private String password;
-    
+
     private String description;
     private String curriculum;
 
